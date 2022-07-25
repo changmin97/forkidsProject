54bd9e7025857c22eb449e0df66ce839d1d4386d
ahnjaehun <77096665+anjaehun@users.noreply.github.com>
2022-07-25 20:21:48 +0900

    app.js test

--- app.js
+++ app.js
@@ -2,7 +2,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const app2 = express();
 const PORT = process.env.PORT;
+const PORT2 = process.env.PORT2;
 const cors = require("cors");
 const connect = require("./database/database.js");
 const morgan = require("morgan");
@@ -55,6 +57,26 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//미들웨어2 
+app2.use(express.json());
+app2.use(cors());
+app2.use(helmet());
+app2.use(morgan("tiny"));
+app2.use(cookieParser());
+app2.use(
+    session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET,
+        cookie: {
+            httpOnly: true,
+            secure: false,
+        },
+    })
+);
+app2.use(passport.initialize());
+app2.use(passport.session());
+
 // 구현 완료 후 라우터 정리
 app.use(
     "/api",
@@ -71,20 +93,56 @@ app.use(
     [mainRouter],
     [searchRouter]
 );
+
+// 구현 완료 후 라우터 정리2
+app2.use(
+    "/api",
+    express.urlencoded({ extended: false }),
+    [recruitPostsRouter],
+    [recruitCommentsRouter],
+    [placePostsRouter],
+    [placeCommentsRouter],
+    [reviewPostsRouter],
+    [reviewCommentsRouter],
+    [chatRoomsRouter],
+    [chatMessagesRouter],
+    [mypagesRouter],
+    [mainRouter],
+    [searchRouter]
+);
+
 app.use("/api/users", express.urlencoded({ extended: false }), [usersRouter]);
 //app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile)); // 스웨거 파일
 
+app2.use("/api/users", express.urlencoded({ extended: false }), [usersRouter]);
+//app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile)); // 스웨거 파일
+
 app.get("/", (req, res) => {
-    res.send("redirect 테스트하기위한 루트 페이지입니다.");
+    res.send("서버1 test");
 });
 
-// 없는 url로 요청한 경우
+app2.get("/", (req, res) => {
+    res.send("서버2 test");
+});
+
+// 1없는 url로 요청한 경우
 app.use((req, res, next) => {
     res.status(404).send("존재하지 않는 url주소 입니다.");
 });
 
-// 서버 에러 핸들링
+// 1서버 에러 핸들링
 app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).send("서버에 에러가 발생하였습니다.");
+});
+
+// 2없는 url로 요청한 경우
+app2.use((req, res, next) => {
+    res.status(404).send("존재하지 않는 url주소 입니다.");
+});
+
+// 2서버 에러 핸들링
+app2.use((error, req, res, next) => {
     console.error(error);
     res.status(500).send("서버에 에러가 발생하였습니다.");
 });
@@ -92,8 +150,14 @@ app.use((error, req, res, next) => {
 const server = http.createServer(app);
 socket(server);
 
+const server2 = http.createServer(app2);
+socket(server2);
+
 server.listen(PORT, () => {
     console.log(`${PORT}번 포트로 서버가 열렸습니다.`);
+});
+server2.listen(PORT2, () => {
+    console.log(`${PORT2}번 포트로 서버가 열렸습니다.`);
 });
 
 //module.exports = app;
